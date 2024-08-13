d72bb4aa38540a0c189ad21040c42897a410dfe5
now-im-inevitable <148607003+now-im-inevitable@users.noreply.github.com>
2024-08-13 19:25:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//calcutor program
+
+const display= document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+
+
+}
+function clearDisplay(){
+    display.value=" ";
+
+
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        alert("Error");
+    }
+
+
+}
